f2cd5086c4946c0320f99b9c464fb0f4faf51abf
Drew Love <davidandrewlove84@gmail.com>
2017-08-31 09:51:00 -0400

    reworking tab functions

--- javascript/app.js
+++ javascript/app.js
@@ -1,62 +1,115 @@
 var main = function(){
-	"use strict"
-
-	var toDos = [
-	"Finish writing this book", 
-	"Take Gracie to the park", 
-	"Answer emails", 
-	"Prep for Monday's class", 
-	"Make up some ToDos", 
-	"Get Groceries"
-	]; 
 
 	$(".tabs a span").toArray().forEach(function (element){
-		var $element = $(element);
+		var $element = $(element); 
+		$content = $(".content"); 
 
 		$element.on("click", function(){
-			var $content, 
-				$input, 
-				$button, 
-				i; 
-
 			$(".tabs a span").removeClass("active"); 
 			$element.addClass("active"); 
-			$("main .content").empty();
-
-			if($element.parent().is(":nth-child(1)")){
-				$content = $("<ul>");
-				for(i=toDos.length-1; i >=0; i--){
-					$content.append($("<li>").text(toDos[i])); 
-				} 
-
-
-
-			} else if ($element.parent().is(":nth-child(2)")){
-				$content = $("<ul>");
-				toDos.forEach(function (todo){
-					$content.append($("<li>").text(todo));	
-				});	
-
-
-						
-			} else if($element.parent().is(":nth-child(3)")){
-				$input = $("<input>"); 
-				$button = $("<button>").text("+");
-
-				$button.on("click", function(){
-					if($input.val() !== ""){
-						toDos.push($input.val()); 
-						$input.val(""); 
-					}
-				}); 
-				$content = $("<div>").append($button, $input); 
-			}
-			
-			$("main .content").append($content); 
-			
-			return false; 
-			}); 
+			$content.empty(); 
+			var $sampleContent = $("<p>TEST</p>")
+			$($content).append($sampleContent); 
+		});
 	});
-	$(".tabs a:first-child span").trigger("click"); 
-};
-$(document).ready(main);
+}; 
+
+$(document).ready(main); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//FUNCTIONAL BLOCK 
+
+// var main = function(){
+// 	"use strict"
+
+// 	var toDos = [
+// 	"Finish writing this book", 
+// 	"Take Gracie to the park", 
+// 	"Answer emails", 
+// 	"Prep for Monday's class", 
+// 	"Make up some ToDos", 
+// 	"Get Groceries"
+// 	]; 
+
+// 	$(".tabs a span").toArray().forEach(function (element){
+// 		var $element = $(element);
+
+// 		$element.on("click", function(){
+// 			var $content, 
+// 				$input, 
+// 				$button, 
+// 				i; 
+
+// 			$(".tabs a span").removeClass("active"); 
+// 			$element.addClass("active"); 
+// 			$("main .content").empty();
+
+// 			if($element.parent().is(":nth-child(1)")){
+// 				$content = $("<ul>");
+// 				for(i=toDos.length-1; i >=0; i--){
+// 					$content.append($("<li>").text(toDos[i])); 
+// 				} 
+
+
+
+// 			} else if ($element.parent().is(":nth-child(2)")){
+// 				$content = $("<ul>");
+// 				toDos.forEach(function (todo){
+// 					$content.append($("<li>").text(todo));	
+// 				});	
+
+
+
+// 			} else if($element.parent().is(":nth-child(3)")){
+// 				$input = $("<input>"); 
+// 				$button = $("<button>").text("+");
+
+// 				$button.on("click", function(){
+// 					if($input.val() !== ""){
+// 						toDos.push($input.val()); 
+// 						$input.val(""); 
+// 					}
+// 				}); 
+// 				$content = $("<div>").append($button, $input); 
+// 			}
+			
+// 			$("main .content").append($content); 
+			
+// 			return false; 
+// 			}); 
+// 	});
+// 	$(".tabs a:first-child span").trigger("click"); 
+// };
+// $(document).ready(main);
