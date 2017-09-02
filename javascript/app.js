09894be3ee0dbd9ca2364ef9181a8fad2bfa71f3
Drew Love <davidandrewlove84@gmail.com>
2017-09-02 15:57:36 -0400

    end of chapter 4

--- javascript/app.js
+++ javascript/app.js
@@ -1,115 +1,62 @@
-var main = function(){
+var main = function () {
+    var toDos = ["Get groceries",
+                 "Make up some new ToDos",
+                 "Prep for Monday's class",
+                 "Answer emails",
+                 "Take Gracie to the park",
+                 "Finish writing this book"];
 
-	$(".tabs a span").toArray().forEach(function (element){
-		var $element = $(element); 
-		$content = $(".content"); 
+    $(".tabs a span").toArray().forEach(function (element) {
+        var $element = $(element);
 
-		$element.on("click", function(){
-			$(".tabs a span").removeClass("active"); 
-			$element.addClass("active"); 
-			$content.empty(); 
-			var $sampleContent = $("<p>TEST</p>")
-			$($content).append($sampleContent); 
-		});
-	});
-}; 
+        // create a click handler for this element
+        $element.on("click", function () {
+            var $content,
+                $input,
+                $button,
+                i;
 
-$(document).ready(main); 
+            $(".tabs a span").removeClass("active");
+            $element.addClass("active");
+            $("main .content").empty();
 
+            if ($element.parent().is(":nth-child(1)")) {
+                // newest first, so we have to go through
+                // the array backwards
+                $content = $("<ul>");
+                for (i = toDos.length-1; i >= 0; i--) {
+                    $content.append($("<li>").text(toDos[i]));
+                }
+            } else if ($element.parent().is(":nth-child(2)")) {
+                // oldest first, so we go through the array forwards
+                $content = $("<ul>");
+                toDos.forEach(function (todo) {
+                    $content.append($("<li>").text(todo));
+                });
+            } else if ($element.parent().is(":nth-child(3)")) {
+                // input a new to-do
+                $input = $("<input>"),
+                $button = $("<button>").text("+");
 
+                $button.on("click", function () {
+                    if ($input.val() !== "") {
+                        toDos.push($input.val());
+                        $input.val("");
+                    }
+                });
 
+                $content = $("<div>").append($input).append($button);
+               /* Alternatively append() allows multiple arguments so the above
+                can be done with $content = $("<div>").append($input, $button); */
+            }
 
+            $("main .content").append($content);
 
+            return false;
+        });
+    });
 
+    $(".tabs a:first-child span").trigger("click");
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//FUNCTIONAL BLOCK 
-
-// var main = function(){
-// 	"use strict"
-
-// 	var toDos = [
-// 	"Finish writing this book", 
-// 	"Take Gracie to the park", 
-// 	"Answer emails", 
-// 	"Prep for Monday's class", 
-// 	"Make up some ToDos", 
-// 	"Get Groceries"
-// 	]; 
-
-// 	$(".tabs a span").toArray().forEach(function (element){
-// 		var $element = $(element);
-
-// 		$element.on("click", function(){
-// 			var $content, 
-// 				$input, 
-// 				$button, 
-// 				i; 
-
-// 			$(".tabs a span").removeClass("active"); 
-// 			$element.addClass("active"); 
-// 			$("main .content").empty();
-
-// 			if($element.parent().is(":nth-child(1)")){
-// 				$content = $("<ul>");
-// 				for(i=toDos.length-1; i >=0; i--){
-// 					$content.append($("<li>").text(toDos[i])); 
-// 				} 
-
-
-
-// 			} else if ($element.parent().is(":nth-child(2)")){
-// 				$content = $("<ul>");
-// 				toDos.forEach(function (todo){
-// 					$content.append($("<li>").text(todo));	
-// 				});	
-
-
-
-// 			} else if($element.parent().is(":nth-child(3)")){
-// 				$input = $("<input>"); 
-// 				$button = $("<button>").text("+");
-
-// 				$button.on("click", function(){
-// 					if($input.val() !== ""){
-// 						toDos.push($input.val()); 
-// 						$input.val(""); 
-// 					}
-// 				}); 
-// 				$content = $("<div>").append($button, $input); 
-// 			}
-			
-// 			$("main .content").append($content); 
-			
-// 			return false; 
-// 			}); 
-// 	});
-// 	$(".tabs a:first-child span").trigger("click"); 
-// };
-// $(document).ready(main);
+$(document).ready(main);
